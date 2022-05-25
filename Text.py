50f233053b4f9f1407c32171ca56aee4d0bcc4e7
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-05-25 18:55:29 +0530

    Create Text.py

---
+++ Text.py
@@ -0,0 +1,12 @@
+START_MESSAGE ="""Hi, {user_name}, 
+My name is {bot_name},  If you have any questions on how to use me,you can contact my creator "<a href='https://t.me/henmark12'> Henmark </a>"
+I'm a id finder bot
+You can find the list of available commands with /help.
+"""
+
+HELP_TEXT ="""Here are the available commands for you.
+you can choose below commands
+/start- Start the conversation
+/help - Return to the help
+/id - Get you chat id
+"""
